db1c05f74ab0544d46baeece144c55bc42e26e49
Ishan Roy <ishanroy3118107@gmail.com>
2025-07-31 20:01:19 +0530

    Fixed errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -12,10 +13,12 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(
   cors({
-    origin: process.env.ORIGIN,
-    credentials: true, 
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
   })
 );
 
@@ -26,9 +29,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.get("/", (_, res) => {
-  res.send("Server is live!");
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: http://localhost:${PORT}`);
