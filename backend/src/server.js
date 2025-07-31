51380551dbd481e8720d78871d5d1f293e21883a
Ishan Roy <ishanroy3118107@gmail.com>
2025-07-31 19:46:06 +0530

    Vercel configs

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -13,12 +12,10 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
-
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
+    origin: process.env.ORIGIN,
+    credentials: true, 
   })
 );
 
@@ -29,13 +26,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("/", (_, res) => {
+  res.send("Server is live!");
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: http://localhost:${PORT}`);
